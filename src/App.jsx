5bcd712f34a1838de386dadb6d441a6711e71823
Andrew Tsegaye <91322467+Andrew-Tsegaye@users.noreply.github.com>
2023-04-25 05:44:12 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="smz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/Andrew-Tsegaye")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles With <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
